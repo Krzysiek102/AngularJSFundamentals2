c2baf0b0764cdbfc34c4a3aabd2f861a94cda4ed
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-20 21:03:40 +0100

    Controllers & markup

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        $scope.sortorder = 'name';
+        $scope.event = {
+            name: 'Angular boot Camp',
+            date: new Date(2013,0,13),
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass introductory',
+                    creatorName: 'Bob Swith',
+                    duration: 1,
+                    level: 'advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0,
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: 2,
+                    level: 'introductory',
+                    abstract: 'This session will take a cleaner look at scopes',
+                    upVoteCount: 0,
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: 4,
+                    level: 'intermediate',
+                    abstract: 'Controllers are the beggining of everithing Angular does',
+                    upVoteCount: 0,
+                }
+            ],
+        };
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        }
+    });
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,13 @@
 'use strict';
+
+eventsApp.filter('durations', function(){
+return function(duration){
+    switch (duration) {
+        case 1: return "Hour";
+        case 2: return "Two hours";
+        case 3: return "Three hours";
+        case 4: return "Half a day";
+        default: duration;
+    }
+}
+})
