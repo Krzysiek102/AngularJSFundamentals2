1034f32daca180dd5e735019301867537c1f75d2
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-21 08:44:14 +0100

    Extract data into json file and access it by $http service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,16 +1,20 @@
 ﻿'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData) {
+    function EventController($scope, eventData, $log) {
         $scope.sortorder = 'name';
-        $scope.event = eventData.getEvent(function(event){
-            $scope.event = event
-        });
+        eventData.getEvent()
+            .success(function (event){
+                $scope.event = event;
+            })
+            .error(function (data, status, headers, config) {
+                $log.warn(data, status, headers, config);
+            });
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
         };
 
         $scope.downVoteSession = function (session) {
             session.upVoteCount--;
-        }
+        };
     });
