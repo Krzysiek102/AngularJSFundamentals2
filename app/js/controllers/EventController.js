46e132b513232962b8433cbb53a1c9ba41742afb
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-21 08:57:11 +0100

    Using promise in getting event

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,6 +3,17 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData, $log) {
         $scope.sortorder = 'name';
+        eventData.getEvent().
+            $promise.then(
+                function (event) {
+                    $scope.event = event;
+                    console.log(event);
+                },
+                function (response) {
+                    console.log(response);
+                }
+            );
+
         $scope.event = event = eventData.getEvent();
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
