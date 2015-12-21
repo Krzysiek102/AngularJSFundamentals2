12bfd2f4ceda6c1288cd787817a8f452eede3dfc
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-21 09:09:36 +0100

    Saving resource works (with static id)

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,10 +1,18 @@
 ﻿'use strict';
 
 eventsApp.controller('EditEventController',
-    function EditEventController($scope) {
+    function EditEventController($scope, eventData) {
         $scope.saveEvent = function (event, newEventForm) {
             if (newEventForm.$valid) {
-                window.alert('event ' + event.name + ' saved');
+                eventData.saveEvent(event)
+                .$promise.then(
+                    function (response) {
+                        console.log('success', response);
+                    },
+                    function (response) {
+                        console.log('failure', response);
+                    }
+                );
             }
         };
         $scope.cancelEdit = function () {
