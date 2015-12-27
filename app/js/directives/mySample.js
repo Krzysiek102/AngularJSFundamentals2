59b48296af934228f23348eae81f94da05ee659d
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-27 17:35:57 +0100

    Extract event thumbnail into directive

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-eventsApp.directive('mySample', function ($compile) {
+eventsApp.directive('mySample', function () {
     return {
         restrict: 'E',
         template: "<input type='text' ng-model='sampleData' />{{sampleData}}<br/>"
