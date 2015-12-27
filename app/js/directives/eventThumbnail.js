de386c77f25749af7493c8da02b42b15f87ac349
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-27 22:19:20 +0100

    Add replace parameter

--- app/js/directives/eventThumbnail.js
+++ app/js/directives/eventThumbnail.js
@@ -3,6 +3,7 @@
 eventsApp.directive('eventThumbnail', function () {
     return {
         restrict: 'E',
+        replace: true,
         templateUrl: '/templates/directives/eventThumbnail.html', 
     };
 });
