85b0ee2b2213f18529a89ca10b9c6dc95ceeeff4
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-12-29 18:22:39 +0100

    Sharing directives via controller

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -1,18 +1,41 @@
 ﻿'use strict';
 
-eventsApp.directive('greeting', function () {
+eventsApp
+    .directive('greeting', function () {
     return {
         restrict: 'E',
         replace: true,
         template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-        controller: '@',
-        name: 'ctrl'
-    };
-});
-
-eventsApp.controller('GreetingController',
-    function GreetingController($scope){
-        $scope.sayHello = function () {
-            alert('Hello');
+        controller: function ($scope) {
+            var greetings = ['hello'];
+            $scope.sayHello = function () {
+                alert(greetings.join());
+            }
+            this.addGrettings = function (greeting) {
+                greetings.push(greeting);
+            }
         }
-    });
+    };
+    })
+.directive('finnish', function () {
+    return {
+        restrict: 'A',
+        require: 'greeting',
+        link: function (scope, element, attrs, controller) {
+            controller.addGrettings('hei');
+        }
+    }
+})
+.directive('hindi', function () {
+    return {
+        restrict: 'A',
+        require: 'greeting',
+        link: function (scope, element, attrs, controller) {
+            controller.addGrettings('hei in Hindi');
+        }
+    }
+})
+
+
+;
+
